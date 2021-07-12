a62013ea7b00272d94e324153f3689bdd0f45eec
ishowta <ishowta@gmail.com>
2021-07-12 18:43:42 +0900

    feat: タイマー機能を追加

--- chatbot.ts
+++ chatbot.ts
@@ -3,6 +3,12 @@ import conf from "config-reloadable";
 import { writeFileSync } from "fs";
 import player from "node-wav-player";
 import schedule from "node-schedule";
+import readline from "readline";
+
+process.stdin.setEncoding("utf8");
+const reader = readline.createInterface({
+  input: process.stdin,
+});
 
 const config = conf();
 
@@ -38,4 +44,12 @@ const every10sec = "0,10,20,30,40,50 * * * * *";
 const every15min = "0 0,15,30,45 * * * *";
 schedule.scheduleJob(every15min, (fireDate) => {
   speak(`現在の時刻は、${`${fireDate}`.substr(16, 8)}です。`);
+});
+
+reader.on("line", (message) => {
+  if (message.match(/^\d+分$/)) {
+    const time = parseInt(message.substr(0, message.length - 1));
+    speak(`${time}分経ったら教えます。`);
+    setTimeout(() => speak(`${time}分経ちました。`), time * 60 * 1000);
+  }
 });
